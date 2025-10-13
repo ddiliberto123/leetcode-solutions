dabc9ccc7b360ea07fc27f3b804991c2a413499c
Daniel Diliberto <daniel.diliberto@hotmail.com>
2025-10-12 23:20:37 -0400

    Time: 0 ms (100.00%) | Memory: 55.6 MB (41.28%) - LeetSync

--- 27-remove-element/remove-element.ts
+++ 27-remove-element/remove-element.ts
@@ -1,14 +1,11 @@
 function removeElement(nums: number[], val: number): number {
-    let count = 0;
     let rightPtr = nums.length - 1;
     for (let i = 0; i <= rightPtr; i++) {
-        count++;
         if (nums[i] == val) {
-            count--;
             nums[i] = nums[rightPtr];
             rightPtr--;
             i--;
         }
     }
-    return count;
+    return rightPtr + 1;
 };
