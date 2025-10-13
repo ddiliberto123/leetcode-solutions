642c9ea04e64cf69d04fd09e53ccdd0285a25ddf
Daniel Diliberto <daniel.diliberto@hotmail.com>
2025-10-13 11:39:18 -0400

    Time: 27 ms (21.17%) | Memory: 60 MB (38.47%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -1,24 +1,7 @@
 function isAnagram(s: string, t: string): boolean {
     if (s.length != t.length)
         return false
-    let letters = new Map<string, number>();
-    for (let i = 0; i < s.length; i++) {
-        if (letters.get(s[i])) {
-            letters.set(s[i], letters.get(s[i]) + 1)
-        } else {
-            letters.set(s[i], 1);
-        }
-    }
-    for (let i = 0; i < t.length; i++) {
-        if (letters.get(t[i])) {
-            letters.set(t[i], letters.get(t[i]) - 1)
-        } else {
-            return false
-        }
-    }
-    for (const pair of letters.entries()) {
-        if (pair[1] != 0)
-            return false;
-    }
-    return true
+    const str1 = [...s].sort();
+    const str2 = [...t].sort();
+    return str1.join('') === str2.join('')
 };
