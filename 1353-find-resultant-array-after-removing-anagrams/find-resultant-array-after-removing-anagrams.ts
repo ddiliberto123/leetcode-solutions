e2fab7322f060692920539051cf247065f7968a0
Daniel Diliberto <daniel.diliberto@hotmail.com>
2025-10-13 11:51:57 -0400

    Time: 6 ms (86.21%) | Memory: 60.9 MB (72.41%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.ts
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.ts
@@ -1,6 +1,6 @@
 function removeAnagrams(words: string[]): string[] {
     for (let i = 0; i < words.length; i++) {
-        if (i < words.length - 1 && checkAnagram(words[i], words[i+1])) {
+        if (i < words.length - 1 && isAnagram(words[i], words[i+1])) {
             words.splice(i+1,1);
             i--
         }
@@ -8,27 +8,18 @@ function removeAnagrams(words: string[]): string[] {
     return words;
 };
 
-function checkAnagram(word1: string, word2: string): boolean {
-    if (word1.length != word2.length)
+function isAnagram(s: string, t: string): boolean {
+    if (s.length != t.length)
         return false
-    let letters = new Map<string, number>();
-    for (let i = 0; i < word1.length; i++) {
-        if (letters.get(word1[i])) {
-            letters.set(word1[i], letters.get(word1[i]) + 1)
-        } else {
-            letters.set(word1[i], 1);
-        }
+    let sCounter: number[] = new Array(26).fill(0)
+    let tCounter: number[] = new Array(26).fill(0)
+    for (let i = 0; i < s.length; i++) {
+        sCounter[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        tCounter[t.charCodeAt(i) - 'a'.charCodeAt(0)]++;
     }
-    for (let i = 0; i < word2.length; i++) {
-        if (letters.get(word2[i])) {
-            letters.set(word2[i], letters.get(word2[i]) - 1)
-        } else {
+    for (let i = 0; i < 26; i++) {
+        if (sCounter[i] != tCounter[i])
             return false
-        }
-    }
-    for (const pair of letters.entries()) {
-        if (pair[1] != 0)
-            return false;
     }
     return true
-}
+};
