a62f9805a466d90f8d21909fa1d56643eae2eb28
Daniel Diliberto <daniel.diliberto@hotmail.com>
2025-10-13 11:03:47 -0400

    Time: 0 ms (100.00%) | Memory: 58.7 MB (90.26%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.ts
+++ 2058-concatenation-of-array/concatenation-of-array.ts
@@ -1,6 +1,8 @@
 function getConcatenation(nums: number[]): number[] {
-    let ans: number[] = []
-    ans.push(...nums)
-    ans.push(...nums)
+    let ans: number[] = new Array(nums.length * 2);
+    for (let i = 0; i < nums.length; i++) {
+        ans[i] = nums[i];
+        ans[i + nums.length] = nums[i]
+    }
     return ans;
 };
